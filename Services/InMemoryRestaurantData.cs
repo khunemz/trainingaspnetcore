43a4717cce53ea1b36ec7edfd8f724953fda7bd5
chutipong roobklom <khunemz@gmail.com>
2019-10-26 21:23:52 +0700

    add context

--- Services/InMemoryRestaurantData.cs
+++ Services/InMemoryRestaurantData.cs
@@ -4,6 +4,32 @@ using OdeToFood.Entities;
 
 namespace OdeToFood.Services
 {
+
+    public class SqlRestaurantData : IRestaurantData {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _context.Restaurants;
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _context.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            _context.Add(newRestaurant);
+            _context.SaveChanges();
+            return newRestaurant;
+        }
+    }
     public class InMemoryRestaurantData : IRestaurantData
     {
         private static List<Restaurant> _restaurant;
