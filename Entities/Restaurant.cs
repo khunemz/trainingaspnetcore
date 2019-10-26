6fad70f32def1d27d6e1e0c0d051baaabdbd9a87
chutipong roobklom <khunemz@gmail.com>
2019-10-26 20:40:58 +0700

    add validation

--- Entities/Restaurant.cs
+++ Entities/Restaurant.cs
@@ -1,4 +1,5 @@
-﻿namespace OdeToFood.Entities
+﻿using System.ComponentModel.DataAnnotations;
+namespace OdeToFood.Entities
 {
 
     public enum CuisineType
@@ -12,9 +13,13 @@
     public class Restaurant
     {
         public int Id  { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required, MaxLength(80)]
+        [Display(Name = "Restaurant Name")]
         public string Name { get; set; }
         public CuisineType Cuisine { get; set; }
-
+        
     }
 
 }
